0c1bc7a83dc9e68adc5d94278b8789f88890d993
blade-demon <xuziwei89@gmail.com>
2022-04-05 16:26:19 +0800

    nestjs config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,7 +8,15 @@ import { PrismaModule } from './prisma/prisma.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule, BookmarkModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    PrismaModule,
+    BookmarkModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
